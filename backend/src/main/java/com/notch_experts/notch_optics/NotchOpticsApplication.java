b0d4e627bae581b4f283574b351fbbdb8c04d00a
syedeasin <syedeasin1000@gmail.com>
2025-01-26 18:33:12 +0600

    Backend Installed

---
+++ backend/src/main/java/com/notch_experts/notch_optics/NotchOpticsApplication.java
@@ -0,0 +1,13 @@
+package com.notch_experts.notch_optics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotchOpticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotchOpticsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/notch_experts/notch_optics/NotchOpticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notch_experts.notch_optics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotchOpticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
